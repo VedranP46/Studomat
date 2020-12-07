abf3384b83d2972a14754c9e22a2e4ef3f536098
--global <vpozaic1103@gmail.com>
2020-12-07 19:39:38 +0100

    Add adress

--- database/migrations/2014_10_10_000000_create_roles_table.php
+++ database/migrations/2014_10_10_000000_create_roles_table.php
@@ -16,7 +16,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            
+            $table->string('adress');
             $table->timestamps();
         });
     }
