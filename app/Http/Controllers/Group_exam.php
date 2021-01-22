b177b2d969ffe098dae898943fe01b29830c938e
VedranP46 <73794908+VedranP46@users.noreply.github.com>
2021-01-22 20:42:48 +0100

    Update Group_exam.php

--- app/Http/Controllers/Group_exam.php
+++ app/Http/Controllers/Group_exam.php
@@ -1,25 +1,86 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
-class Role extends Model
+class group_exam extends Controller
 {
-    use HasFactory;
-    protected $fillable = [
-        'topic_id',
-        'user_id',
-        'joined_at',
-        
-    ];
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $group_exam= group_exam::paginate();
+        return view('group_exams.index', compact ('group_exams')); 
+    }
 
     /**
-     * The users that belong to the role.
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
-    public function users()
+    public function create()
     {
-        return $this->belongsToMany(User::class);
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $group_exam= group_exam::paginate();
+        return view('group_exams.index', compact ('group_exams')); 
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
